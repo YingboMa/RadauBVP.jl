a88a9c6980ce43415a65adcefb79afe7c0cb85c3
mobius-eng <Alexey.Cherkaev@gmail.com>
2016-10-28 15:50:46 +0200

    Optimized working solver

--- src/RadauBVP.jl
+++ src/RadauBVP.jl
@@ -50,60 +50,91 @@ with `jac` being a matrix of size `M×M`: `∂(y[j])[f![i]] = jac[i,j]`.
 
 """
 function radau3(c!, f!, dc!, df!, y0 :: Array{Float64, 2}, t0 :: Float64, tf :: Float64)
-	M, N = size(y0)                            # M - spatial size, N - time
-	MN = M * N                                 # total problem size
-	MN = M * N                                 # total problem size
-	yarg = Array(eltype(y0), M)                # temp location for arg of f!
+	sz = size(y0)                                 # M - spatial size, N - time
+	const M :: Int64 = sz[1]
+	const N :: Int64 = sz[2]
+	const MN :: Int64 = length(y0)                  # total problem size
+	local yarg = Array(eltype(y0), M)               # temp location for arg of f!
+	local y1 :: Vector{Float64} = Array(eltype(y0), M)
+	local yN :: Vector{Float64} = Array(eltype(y0), M)
+	local k1 :: Vector{Float64} = Array(eltype(y0), M)
+	local k2 :: Vector{Float64} = Array(eltype(y0), M)
+	local k3 :: Vector{Float64} = Array(eltype(y0), M)
 	# Time
-    t = linspace(t0, tf, N)                    # all time points
-    h = t[2]-t[1]                              # time step
+    const t = linspace(t0, tf, N)                    # all time points
+    const h :: Float64 = t[2]-t[1]                   # time step
     # Butcher tableau for Radau IIA method (s = 3)
     # all the expressions should be computed at compile time by Julia...
-    local c :: Vector{Float64} = [0.4-√6/10, 0.4+√6/10, 1.0]
-    local a :: Array{Float64,2} = [
+    const c :: Vector{Float64} = [0.4-√6/10, 0.4+√6/10, 1.0]
+    const a :: Array{Float64,2} = [
         11/45-7*√6/360     37/225-169*√6/1800 -2/225+√6/75;
         37/225+169*√6/1800 11/45+7*√6/360     -2/225-√6/75;
         4/9-√6/36          4/9+√6/36           1/9]
-    local b :: Vector{Float64} = [4/9-√6/36, 4/9+√6/36, 1/9]
+    const b :: Vector{Float64} = [4/9-√6/36, 4/9+√6/36, 1/9]
     # Resulting non-linear equation
-    function eq!(x, r)
-        # the total set of equations contains equations on y & k.
-        # spit them, creating the views
-        # (in Julia 0.5, `reshape` does not copy, so, `@view` might not be necessary)
-        y = reshape((@view x[1:MN]), (M,N))
-        k = reshape((@view x[MN+1:end]), (M, 3, N-1))
-		# yarg = Array(eltype(x), M)
-        # info("y = $(y[:,:])")
-        # info("k = $(k[:,:,:])")
+    @inbounds function eq!(x :: Vector{Float64}, r :: Vector{Float64})
+		@inline yind(m :: Int64, n :: Int64) = x[M*(n-1) + m]
+		@inline kind(m :: Int64, j :: Int64, n :: Int64) =
+			x[MN + 3M*(n-1) + M*(j-1) + m]
+		@inline yrange(dest :: Vector{Float64}, n :: Int64) =
+			dest[:] = @view x[M*(n-1)+1:M*(n-1)+M]
+		@inline krange(dest :: Vector{Float64}, i :: Int64, n :: Int64) =
+			dest[:] = @view x[MN+3M*(n-1)+M*(i-1)+1:MN+3M*(n-1)+M*(i-1)+M]
         # Boundary conditions
-        c!((@view y[1:M, 1]), (@view y[1:M, N]), view(r, 1:M))
+		yrange(y1, 1)
+		yrange(yN, N)
+		# not sure if there is a better way to do it...
+        c!(y1, yN, view(r, 1:M))
         # base index
         ind = M
         # Iterate over each time element
         for n=1:(N-1)
-            k1 = @view k[:,1,n]
-            k2 = @view k[:,2,n]
-            k3 = @view k[:,3,n]
-            yn = @view y[:,n]
-            yn1 = @view y[:,n+1]
+			krange(k1, 1, n)
+			krange(k2, 2, n)
+			krange(k3, 3, n)
+			# y[:,n]
+			yrange(yN, n)
+			# y[:,n+1]
+			yrange(y1, n+1)
             # y[:,n+1] = y[:,n] + h∑b[j]*k[:,j,n]
             # this doesn't fuse in Julia 0.5
             # r[ind+1:ind+ny0] .= yn1 .- yn .- h .* (b[1] .* k1 .+ b[2] .* k2 .+ b[3] .* k3)
             # this form should fuse:
-            r[ind+1:ind+M] .= (-).(yn1, (+).(yn, (*).(h, (+).((*).(b[1], k1), (*).(b[2], k2), (*).(b[3], k3)))))
+            r[ind+1:ind+M] .= (-).(y1, (+).(yN, (*).(h, (+).((*).(b[1], k1), (*).(b[2], k2), (*).(b[3], k3)))))
             # update base index
             ind += M
             # k[:,j,n] - f(t[i]+c[j]h, y[:,n]+h∑a[j,q]k[:,q, n]) = 0
-            for j=1:3
-                # doesn't fuse in Julia 0.5
-                # yarg .= yn .+ h .* (a[j,1] .* k1 .+ a[j,2] .* k2 .+ a[j,3] .* k3)
-                yarg .= (+).(yn, (*).(h, (+).((*).(a[j,1], k1), (*).(a[j,2], k2), (*).(a[j,3], k3))))
-                # storage for equation corresponding to individual k[j]
-                rk = @view r[ind+1:ind+M]
-                f!(t[n]+c[j]*h, yarg, rk)
-                rk .= (-).((@view k[:,j,n]), rk)
-                ind += M
-            end
+			# manual loop unrolling: otherwise have to deal with views over
+			# k[:,j]
+			# j = 1
+			yarg .= (+).(yN, (*).(h, (+).((*).(a[1,1], k1), (*).(a[1,2], k2), (*).(a[1,3], k3))))
+			rk = @view r[ind+1:ind+M]
+			f!(t[n]+c[1]*h, yarg, rk)
+			rk .= (-).(k1, rk)
+			ind += M
+			# j = 2
+			yarg .= (+).(yN, (*).(h, (+).((*).(a[2,1], k1), (*).(a[2,2], k2), (*).(a[2,3], k3))))
+			rk = @view r[ind+1:ind+M]
+			f!(t[n]+c[2]*h, yarg, rk)
+			rk .= (-).(k2, rk)
+			ind += M
+			yarg .= (+).(yN, (*).(h, (+).((*).(a[3,1], k1), (*).(a[3,2], k2), (*).(a[3,3], k3))))
+			rk = @view r[ind+1:ind+M]
+			f!(t[n]+c[3]*h, yarg, rk)
+			rk .= (-).(k3, rk)
+			ind += M
+            # for j=1:3
+            #     # doesn't fuse in Julia 0.5
+            #     # yarg .= yn .+ h .* (a[j,1] .* k1 .+ a[j,2] .* k2 .+ a[j,3] .* k3)
+            #     yarg .= (+).(yN, (*).(h, (+).((*).(a[j,1], k1), (*).(a[j,2], k2), (*).(a[j,3], k3))))
+            #     # storage for equation corresponding to individual k[m,j,n]
+			# 	# can't avoid the view here
+            #     rk = @view r[ind+1:ind+M]
+            #     f!(t[n]+c[j]*h, yarg, rk)
+			# 	# not ideal...
+            #     rk .= (-).((@view k[:,j,n]), rk)
+            #     ind += M
+            # end
         end
     end
 
@@ -180,8 +211,32 @@ function radau3(c!, f!, dc!, df!, y0 :: Array{Float64, 2}, t0 :: Float64, tf :: 
     x0[1:MN] = @view y0[:]
 	x0[MN+1:end] = k0[:]
 	# warn("size(x0) = $(size(x0))")
-    # nlsolve(eq!, deq!, x0)
-	(eq!, deq!, x0)
+    result = nlsolve(eq!, deq!, x0)
+	t_tot = zeros(Float64, 2*(N-1)+N)
+	y_tot = zeros(Float64, (M, length(t_tot)))
+	if result.x_converged || result.f_converged
+		yres = reshape(result.zero[1:MN], M, N)
+		kres = reshape(result.zero[MN+1:end], M, 3, (N-1))
+		# Time with all collocation points
+		t_tot[1] = t0
+		for n=1:(N-1)
+			for i=1:2
+				t_tot[1+3*(n-1)+i] = t[n] + c[i]*h
+			end
+			t_tot[1+3*(n-1)+3] = t[n+1]
+		end
+		# Result in all collocation points
+		y_tot[:,1] = yres[:, 1]
+		for n=1:(N-1)
+			for i=1:2
+				y_tot[:,1+3*(n-1)+i] = yres[:,n] + h * (a[i,1]*kres[:,1,n]+a[i,2]*kres[:,2,n]+a[i,3]*kres[:,3,n])
+			end
+			y_tot[:,1+3*(n-1)+3] = yres[:,n+1]
+		end
+		return (true, t_tot, y_tot, result)
+	end
+	return (false, t_tot, y_tot, result)
+	# (eq!, deq!, x0)
 end
 
 
